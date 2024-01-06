e64c12caedaa45c382008080181dd4b4280026d6
3369yu <151356820+3369yu@users.noreply.github.com>
2024-01-06 07:14:53 +0000

    [Finish]userのshow,edit,index,update追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,50 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def index
+    @book = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+       books = Book.all
+      render :index
+    end
+  end
+
+  def updete
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to @book
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'
+    end
+  end
+
+  private
+
+  def book_params
+  params.require(:book).permit(:Title, :Opinion)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
 class UsersController < ApplicationController
+  
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def index
+    @users = User.all
+  end
+  
+  def updete
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to @user
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introductin, :profile_image)
   end
 end
